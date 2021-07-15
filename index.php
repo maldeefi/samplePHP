956ca997259dc6778056c0ec5b6e317f27cf99a2
maldeefi <alinisham@live.com>
2021-07-16 02:04:57 +0500

    With a Form

--- index.php
+++ index.php
@@ -10,3 +10,21 @@ $num1 = 100;
 $num2 = 5;
 echo $num1 * $num2;
 ?>
+
+<form action="index.php" method="post">
+    First name: <input type="text" name="fname" value="Input value here">
+</form>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Welcome <?php echo $_POST["fname"]; ?>
+
+</body>
+</html>
